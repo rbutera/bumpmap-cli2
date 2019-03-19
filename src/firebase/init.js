5b7e9d0c4996a240972527d818350a193852ea5a
Rai Butera <rai@rbutera.com>
2019-03-19 16:44:01 +0000

    added animated logo

---
+++ src/firebase/init.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyA7iHfY6FaI4HptBHdBJN7aBlMpeVk8w_g',
+  authDomain: 'geobump-app.firebaseapp.com',
+  databaseURL: 'https://geobump-app.firebaseio.com',
+  projectId: 'geobump-app',
+  storageBucket: 'geobump-app.appspot.com',
+  messagingSenderId: '224309728249',
+}
+
+export const firebaseApp = firebase.initializeApp(config)
+
+firebaseApp.firestore().settings({ timestampsInSnapshots: true })
+
+const db = firebaseApp.firestore()
+
+export default db
