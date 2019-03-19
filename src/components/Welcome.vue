04071ae404034e35d84632f5df16d0daed0bec59
Rai Butera <rai@rbutera.com>
2019-03-19 17:44:33 +0000

    initial version

---
+++ src/components/Welcome.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="welcome container">
+    <img src="/static/logo/logo-square_animated.svg" alt="GeoBump logo">
+    <h1>Welcome</h1>
+    <p>
+      Welcome to
+      <strong>geobump</strong>.
+    </p>
+    <p>Here you can post notices pinned to a location.</p>
+    <p>You can pin public notices to any location, including your current one.</p>
+    <p>Other
+      <strong>geobump users</strong> can see/respond to your notices by replying to them or by sending you a private message.
+    </p>
+    <p>You can use this site anonymously and/or with a username that you choose.</p>
+    <p>The notices will be visible for 7 days.</p>
+    <p>After this time has passed, the notices will be archived.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
