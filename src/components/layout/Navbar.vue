a66d394e06fb1df640b77a68e4ac2e2707b53d13
Rai Butera <rai@rbutera.com>
2019-03-20 10:33:14 +0000

    add release phase

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,21 +2,18 @@
   <div class="navbar">
     <nav class="gray darken-4 geobump-navbar">
       <div class="container">
-        <router-link :to="{name: 'Map'}" class="brand-logo left">
-          <img
-            class="brand-logo-emblem"
-            src="/static/logo/emblem-light.svg"
-            height="30"
-            alt="geobump logo"
-          >
-          <img
-            class="brand-logo-image"
-            src="/static/logo/geobump-light.svg"
-            height="50"
-            alt="geobump"
-          >
-        </router-link>
-        <span class="left version">v{{version}}</span>
+        <div class="brand-logo left">
+          <router-link :to="{name: 'Map'}">
+            <img class="brand-logo-emblem" src="/static/logo/emblem-light.svg" alt="geobump logo">
+            <img class="brand-logo-image" src="/static/logo/geobump-light.svg" alt="geobump">
+          </router-link>
+          <span class="version">
+            <span class="version-label">v</span>
+            <span class="version-number">{{version}}</span>
+            <span class="version-release" v-if="release.phase">{{release.phase}}</span>
+          </span>
+        </div>
+
         <ul class="right">
           <li>
             <router-link :to="{name: 'Join'}">Join</router-link>
@@ -35,9 +32,10 @@ import geobumpAppData from '@/lib/app-data'
 export default {
   name: 'Navbar',
   data() {
-    const { version } = geobumpAppData
+    const { version, release } = geobumpAppData
     return {
       version,
+      release,
     }
   },
 }
@@ -45,9 +43,14 @@ export default {
 
 <style lang="scss">
 .navbar {
+  .brand-logo-emblem {
+    height: 30px;
+  }
+
   .brand-logo-image {
+    height: 40px;
     position: relative;
-    top: 10px;
+    top: 7px;
   }
 
   .brand-logo-image,
@@ -57,7 +60,42 @@ export default {
   }
 
   .gray.darken-4.geobump-navbar {
-    background-color: rgba(50, 50, 50, 0.8);
+    background-color: rgba(20, 20, 20, 0.8);
+  }
+
+  .brand-logo {
+    user-select: none;
+    .version {
+      cursor: help;
+      display: inline-block;
+      margin: 0;
+      padding: 0;
+      font-size: 0.5em;
+      font-weight: 150;
+      line-height: 64px;
+      vertical-align: top;
+      letter-spacing: 2px;
+      color: rgba(255, 255, 255, 0.25);
+      user-select: none;
+    }
+    .version-number,
+    .version-label {
+      display: inline-block;
+      margin: 0;
+      padding: 0;
+    }
+    .version-label {
+      color: rgba(255, 255, 255, 0.15);
+      font-size: 0.9em;
+    }
+    .version-number {
+      margin-left: -5px;
+    }
+    .version-release {
+      font-size: 0.75em;
+      font-weight: 500;
+      letter-spacing: 0px;
+    }
   }
 }
 </style>
--- src/lib/app-data.js
+++ src/lib/app-data.js
@@ -2,4 +2,7 @@ const version = require('../../package.json').version
 
 export default {
   version,
+  release: {
+    phase: 'ALPHA',
+  },
 }
